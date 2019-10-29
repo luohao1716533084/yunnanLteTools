5fbe1acb259f5f0ffb79fefc4acca7a63b9ddb38
luohao1716533084 <luohao1990@yeah.net>
2019-10-29 16:29:32 +0800

    DfOperate class

--- Angle_tool.py
+++ Angle_tool.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 colName = ['区县', 'eNodeBName', 'CellName', '站点类型', 'EARFCN', 'Azimuth']
-excelPath = r'C:\\Users\\luohao\\Desktop\\事件工具\\昆明华为LTE工参-20191021.xlsx'
+excelPath = r'C:\\Users\\luohao\\Desktop\\事件工具\\昆明华为LTE工参-20191028.xlsx'
 sheetName = '华为'
 AREA, CATE = '盘龙', '宏站'
 
@@ -25,15 +25,26 @@ class DfOperate:
 	def __init__(self, df):
 		self.df = df
 
-	def get_cellname(self, colName):                  #获取需要的字段的去重值
-		self.colName_list = list(set(self.df[colName]))
+	def get_cellname(self, col_Name):                  #获取需要的字段的去重值
+		self.colName_list = list(set(self.df[col_Name]))
 		return self.colName_list
 
+	def filter_field(self, field_value):
+		df = self.df[self.df['eNodeBName'] == field_value]
+		return df
 
-excel = OpenExcel(excelPath, sheetName, colName)
 
-print(excel.pre_process(AREA, CATE))
-# def main():
-# 	df = excel.pre_process('盘龙', '宏站')
+def main():
+	excel = OpenExcel(excelPath, 'test', colName)
+	df = excel.pre_process(AREA, CATE)
+	dfoperate = DfOperate(df)                                #生成一个DfOperate
+	ENodeBName_List = dfoperate.get_cellname('eNodeBName')
+	for ENodeBName in ENodeBName_List:
+		sub_tmp = dfoperate.filter_field(ENodeBName)
+		#pivot_table = pd.pivot_table(sub_tmp, values=['eNodeBName', 'CellName', 'Azimuth'], columns=['eNodeBName', 'CellName', 'Azimuth'], index='EARFCN')
+		print(sub_tmp.stack())
 
+
+if __name__ == '__main__':
+	main()
 
