dcd24467c993f6c95742ca31201aacb888853b6f
LuoHao <luohao1990@yeah.net>
2020-06-05 10:34:01 +0800

    大数据平台登录

--- YN_bigdata_platform_receive.py
+++ YN_bigdata_platform_receive.py
@@ -1,17 +1,59 @@
 #coding:utf-8
 
 from selenium import webdriver
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 url = "http://10.174.240.17:8081/portal/pure/Frame.action"
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome()                         #驱动浏览器
 browser.get(url)
 input_username = browser.find_element_by_id("username")
-input_username.send_keys("hechunfen")
+input_username.send_keys("km_hw_shaoguohua")
 input_password = browser.find_element_by_id("password")
-input_password.send_keys("hechunfen0304")
+input_password.send_keys("Password11!")
 button = browser.find_element_by_class_name("btn-submit")
 button.click()
-
 button_sys_notice = browser.find_element_by_class_name("sys-window-btn-close")
 button_sys_notice.click()
+
+# 选择在线协作
+# zaixianxiezuo = browser.find_element_by_xpath('//*[@id="node_ZXXZ"]/i')
+# zaixianxiezuo.click()
+
+#定位到要双击的元素
+qqq =browser.find_element_by_xpath('//*[@id="node_ZXXZ"]/i')
+#对定位到的元素执行鼠标双击操作
+ActionChains(browser).double_click(qqq).perform()
+
+
+
+# 中端优化市到三方
+browser.switch_to.frame("link1")
+
+
+time.sleep(5)                                                                                                          #强制睡眠10s
+move = browser.find_element_by_xpath('/html/body/div[1]/div/div/div')
+ActionChains(browser).move_to_element(move).perform()
+input_gongdanNum = browser.find_element_by_xpath('//*[@id="public_inputCompinent__inputMainorderCode"]')                #定位工单号输入框
+input_gongdanNum.send_keys("ZDYH-KM-CG064-20200511-01857432")                                                           #输入工单号
+
+
+search_gongdan = browser.find_element_by_id('task_management_mat')                  #定位工单搜索框
+search_gongdan.click()                                                              #点击搜索框
+
+# 任务处理
+
+
+time.sleep(5)
+# 操作按钮
+data = browser.find_element_by_class_name("tableDropdown")
+data.click()
+
+#任务处理
+taskProcButton = browser.find_element_by_xpath('/html/body/ul/div[2]/li/i')
+taskProcButton.click()
+
+dataTime = browser.find_element_by_xpath('//*[@class="el-tabs__nav-scroll"]/div')
--- selenium_learn.py
+++ selenium_learn.py
@@ -1,44 +1,37 @@
 #coding:utf-8
 
-"""
-selenium 基本操作
-"""
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from tomorrow import threads
+import pandas
+import os
 
-brower = webdriver.Chrome()
 
-try:
-    brower.get("https://www.baidu.com")
-    input = brower.find_element_by_id("kw")                     #kw应该是收缩框
-    input.send_keys("python")
-    input.send_keys(Keys.ENTER)
-    wait = WebDriverWait(brower, 10)
-    wait.until(EC.presence_of_element_located((By.ID, "content_left")))
-    print(brower.current_url)
-    print(brower.get_cookies())
-    print(brower.page_source)
-finally:
-    brower.close()
+URL = "http://10.174.240.17:8081/portal/pure/Frame.action"
 
-"""
-声明浏览器对象
-from selenium import webdriver
+@threads(5)
+def startBrowser(url):
+    browser = webdriver.Chrome()                         #驱动浏览器
+    browser.get(url)
+    input_username = browser.find_element_by_id("username")
+    input_username.send_keys("km_hw_shaoguohua")
+    input_password = browser.find_element_by_id("password")
+    input_password.send_keys("Password11!")
+    button = browser.find_element_by_class_name("btn-submit")
+    button.click()
+    button_sys_notice = browser.find_element_by_class_name("sys-window-btn-close")
+    button_sys_notice.click()
 
-browser = webdriver.Chrome()
-browser = webdriver.Firefox()
-browser = webdriver.Edge()
-browser = webdriver.PhantomJS()
-browser = webdriver.Safari()
-"""
+def get_receive_num():
+    path = os.getcwd()
+    dirs = os.listdir(path)
+    """
+    找到EUtranCellTDD, EUtranReselectionTDD文件路径，添加至file_file
+	"""
+    file_path = []
+    for i in dirs:
+        if "接单" in i:
+            file_path.append(i)
 
-"""
-访问页面
-from selenium import 
-
-"""
-
+if __name__ == '__main__':
+    for i in range(5):
+        startBrowser(URL)
